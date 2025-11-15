cc1158dc3eadea19d6cc44d972d3aee3c99ff66e
ramarier11 <ramarier11@gmail.com>
2025-11-15 08:20:28 -0500

    Update ProfessionTracker.lua
    debugging- begins

--- ProfessionTracker.lua
+++ ProfessionTracker.lua
@@ -885,6 +885,7 @@ end
 -- Check One-Time Treasures for a Character's Profession
 -- ============================================================
 function ProfessionTracker:EvaluateOneTimeTreasures(charKey, profID, expIndex)
+    print("One Time Treasure - running")
     local charData = ProfessionTrackerDB.characters[charKey]
     if not charData or not charData.professions then return end
 
