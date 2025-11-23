220390009a49d2f836045e194bd90d4f1aaa3b2c
ramarier11 <ramarier11@gmail.com>
2025-11-23 11:54:05 -0500

    Update ProfessionTrackerUI.lua

--- ProfessionTrackerUI.lua
+++ ProfessionTrackerUI.lua
@@ -340,7 +340,7 @@ function ProfessionTrackerDashboard:CreateProfessionProgress(parentEntry, profNa
     -- Crafting Order status
     table.insert(statusLines, GetStatusLine(
         craftingOrderIcon,
-        "Crafting Order",
+        "Profession Quest",
         weekly.craftingOrderQuest == true,
         true  -- isAtlas = true
     ))
