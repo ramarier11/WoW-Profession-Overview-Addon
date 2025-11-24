d2d1b86e530eff8ee1bd33399ee089d325571913
ramarier11 <ramarier11@gmail.com>
2025-11-24 10:41:07 -0500

    Update CharacterDetailWindow.lua

--- CharacterDetailWindow.lua
+++ CharacterDetailWindow.lua
@@ -4,7 +4,7 @@
 
 -- Create the detail window frame
 local CharacterDetailWindow = CreateFrame("Frame", "ProfessionTrackerCharacterDetail", UIParent, "BackdropTemplate")
-CharacterDetailWindow:SetSize(400, 250)
+CharacterDetailWindow:SetSize(400, 270)
 CharacterDetailWindow:SetPoint("CENTER")
 CharacterDetailWindow:Hide()
 CharacterDetailWindow:SetFrameStrata("DIALOG")
