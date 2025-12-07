9174e17837bb2dcff52229778d0659c7da0d404c
ramarier11 <ramarier11@gmail.com>
2025-12-07 13:29:38 -0500

    Update CharacterDetailWindow.lua

--- CharacterDetailWindow.lua
+++ CharacterDetailWindow.lua
@@ -184,7 +184,7 @@ function CharacterDetailWindow:RefreshDisplay()
         separator:SetPoint("TOPLEFT", faireInfo, "BOTTOMLEFT", 0, -5)
         --separator:SetText("----------------------------------")
         separator:SetTextColor(0.5, 0.5, 0.5, 1)
-        CharacterDetailWindow:SetSize(400, characterDetailWindowHeight + 50)
+        CharacterDetailWindow:SetSize(400, characterDetailWindowHeight + 30)
         yOffset = -50
     end
     
