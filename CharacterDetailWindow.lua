0342ebe05e0edb2925b9319d913c09d9faad7feb
ramarier11 <ramarier11@gmail.com>
2025-12-07 13:21:28 -0500

    Update CharacterDetailWindow.lua

--- CharacterDetailWindow.lua
+++ CharacterDetailWindow.lua
@@ -185,7 +185,7 @@ function CharacterDetailWindow:RefreshDisplay()
         --separator:SetText("----------------------------------")
         separator:SetTextColor(0.5, 0.5, 0.5, 1)
         
-        yOffset = -100
+        yOffset = -60
     end
     
     -- Build detailed profession display (2-column layout)
